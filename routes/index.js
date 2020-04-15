6569e83d4dfa80d92c3dc0219b28824d6c01a582
Keyue HUANG <keyueh@student.unimelb.edu.au>
2020-04-16 00:38:48 +1000

    1
    1

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'COVID-19 Data Of Melbourne' });
+});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var login = require('../controller/authenticate/login');
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  var users = [{
+    name : 'admin'
+  }];
+
+  res.send(users);
+
+});
+
+router.post('/login',function (req,res,next){
+
+  const username =  req.body.username;
+  var loginResult = login(username, req.body.password);
+
+  if(loginResult){
+    res.render('users',{username: username});
+  }
+  else {
+    res.render('index',{error: true});
+  }
+});
+
+module.exports = router;
