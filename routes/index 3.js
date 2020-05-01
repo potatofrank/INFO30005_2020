894950b3a8fcb7aa43a4b59aefcecfa5e0ad1d2f
Keyue HUANG <keyueh@student.unimelb.edu.au>
2020-05-01 16:55:29 +1000

    submit
    submit

---
+++ routes/index 3.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Team Ultra Protection' });
+});
+
+router.post('/log',function (req,res,next) {
+  res.render('loginPage', { title: 'Admin Login' });
+});
+
+module.exports = router;
