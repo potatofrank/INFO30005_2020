0a0c1e5606c974e5468012a9ed2e7da0a265ca3f
Keyue HUANG <keyueh@student.unimelb.edu.au>
2020-05-13 00:47:25 +1000

    1
    1

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Team Ultra Protection' });
+  res.render('H-Home', { title: 'Team Ultra Protection' });
 });
 
 router.get('/log',function (req,res,next) {
   res.render('loginPage', { title: 'Admin Login' });
+});
+
+router.get('/contact',function (req,res,next) {
+  res.render('H-contact');
+});
+
+router.get('/Home',function (req,res,next) {
+  res.render('H-Home');
 });
 
 module.exports = router;
