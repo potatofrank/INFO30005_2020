887c6580cfdc98322dd3a45d5e10b6955cbf6293
potatofrank <yichaoy1@student.unimelb.edu.au>
2020-06-07 20:31:06 +1000

    test

--- controller/totalCaseController.js
+++ controller/totalCaseController.js
@@ -23,23 +23,24 @@ var findlatesttotalCase = function (req,res,next) {
 
 var createTotalCase = function(req, res, next) {
     var item = {
-        Date:req.body.Date,
-        Accumulate_Confirmed_Cases:req.body.Accumulate_Confirmed_Cases,
-        Current_Existing_Cases:req.body.Current_Existing_Cases,
-        Accumulate_Deaths:req.body.Accumulate_Deaths,
-        Accumulate_Cured_Cases:req.body.Accumulate_Cured_Cases
+        Date: req.body.Date,
+        Accumulate_Confirmed_Cases: req.body.Accumulate_Confirmed_Cases,
+        Current_Existing_Cases: req.body.Current_Existing_Cases,
+        Accumulate_Deaths: req.body.Accumulate_Deaths,
+        Accumulate_Cured_Cases: req.body.Accumulate_Cured_Cases
     };
     var data = new totalCase(item);
-    data.save();
+    res.status(200).send();
     //res.render('A-totalCaseTable', {output1: 'Submit Successfully'});
+    res.status(200).send({});
 };
 
 
 var deleteTotalCase = function(req, res, next) {
     var id = req.body.id;
     console.log(id);
-    totalCase.findOneAndDelete(id).exec();
-    //res.render('A-totalCaseTable', {output2: 'Delete Successfully'});
+    totalCase.findByIdAndDelete(id).exec();
+    res.render('A-Home');
 };
 
 module.exports.findAllTotalCase = findAllTotalCase;
